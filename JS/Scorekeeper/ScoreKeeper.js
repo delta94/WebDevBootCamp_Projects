8f3140feb3a24b0e9c13d7d402755b6b464fd6fe
Piyali Dey <piyalidey@MacBook-Pro.local>
2017-02-10 16:52:26 -0800

    A simple JS project of creating a scoreboard using Events

---
+++ JS/Scorekeeper/ScoreKeeper.js
@@ -0,0 +1,57 @@
+//alert("Connected!")
+
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var resetButton = document.getElementById ("reset");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
+var p1display= document.querySelector("#p1display");
+var p2display= document.querySelector("#p2display");
+
+var p1Score = 0;
+var p2Score = 0;
+
+var gameOver = false;
+var winningScore = 5;
+
+p1Button.addEventListener("click", function(){
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === winningScore) {
+            p1display.classList.add("winner");
+            gameOver = true;
+        }
+        p1display.textContent = p1Score;
+    }
+});
+
+p2Button.addEventListener("click", function(){
+    if (!gameOver) {
+        p2Score++;
+        if (p2Score === winningScore) {
+            p2display.classList.add("winner");
+            gameOver = true;
+        }
+    p2display.textContent = p2Score;
+    }
+});
+
+resetButton.addEventListener ("click", function () {
+    reset();
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1display.textContent = 0;
+    p2display.textContent = 0;
+    p1display.classList.remove("winner");
+    p2display.classList.remove("winner");
+    gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
+});
