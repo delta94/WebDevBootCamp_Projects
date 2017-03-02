8fb6e3f946b5f7101a155c2d1196f28108b9ab35
Piyali Dey <pdey@ncsu.edu>
2017-03-01 18:36:21 -0800

    v5 of Project YelpCamp. Added public directory and custom stylesheet

--- Project_YelpCamp/app.js
+++ Project_YelpCamp/app.js
@@ -5,82 +5,65 @@ var express     = require("express"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     seedDB      = require("./seeds")
-
-seedDB();
-mongoose.connect("mongodb://localhost/yelp_camp")
+    
+mongoose.connect("mongodb://localhost/yelp_camp_v4");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
 
-
-/*Campground.create(
-     {
-         name: "Mountain Goat's Rest", 
-         image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg",
-         description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
-         
-     },
-     function(err, campground){
-      if(err){
-          console.log(err);
-      } else {
-          console.log("NEWLY CREATED CAMPGROUND: ");
-          console.log(campground);
-      }
-    });*/
-
-// Routes
-// landing page
-app.get("/", function(req, res) {
+app.get("/", function(req, res){
     res.render("landing");
 });
 
-// INDEX - show all campgrounds
-app.get("/campgrounds", function(req, res) {
+//INDEX - show all campgrounds
+app.get("/campgrounds", function(req, res){
     // Get all campgrounds from DB
-    Campground.find({}, function(err, allcampgrounds) {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds/index", {campgrounds:allcampgrounds});
-        }
+    Campground.find({}, function(err, allCampgrounds){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("campgrounds/index",{campgrounds:allCampgrounds});
+       }
     });
 });
 
-// CREATE - add new campground to the DB
-app.post("/campgrounds", function(req, res) {
+//CREATE - add new campground to DB
+app.post("/campgrounds", function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
-    var image= req.body.image;
+    var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var newCampground = {name: name, image: image, description: desc}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
-        if(err) {
+        if(err){
             console.log(err);
         } else {
-            // redirect back to campgrounds page
+            //redirect back to campgrounds page
             res.redirect("/campgrounds");
         }
     });
 });
 
-// NEW - show form to create new campground
-app.get("/campgrounds/new", function(req, res) {
-    res.render("campgrounds/new");
+//NEW - show form to create new campground
+app.get("/campgrounds/new", function(req, res){
+   res.render("campgrounds/new"); 
 });
 
-// SHOW - show more info about one campground
+// SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
     //find the campground with provided ID
     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
             console.log(err);
         } else {
+            console.log(foundCampground)
             //render show template with that campground
             res.render("campgrounds/show", {campground: foundCampground});
         }
     });
-})
+});
 
 
 // ====================
@@ -121,9 +104,6 @@ app.post("/campgrounds/:id/comments", function(req, res){
    //redirect campground show page
 });
 
-
-
-// Tell Express to listen for requests (start server)
 app.listen(3000, function(){
     console.log("Starting the YelpCamp Server on port 3000");
 });
