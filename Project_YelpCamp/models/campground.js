8b294958d00da956b61d1a1ee16b150bdb508996
Piyali Dey <pdey@ncsu.edu>
2017-03-06 11:25:39 -0800

    v7 of Project YelpCamp/ Cleaning Up the code

--- Project_YelpCamp/models/campground.js
+++ Project_YelpCamp/models/campground.js
@@ -1,11 +1,10 @@
 var mongoose = require("mongoose");
 
-// Schema Setup
-var campgroundSchema = new mongoose.Schema ({
-    name: String,
-    image: String, 
-    description: String,
-    comments: [
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String,
+   comments: [
       {
          type: mongoose.Schema.Types.ObjectId,
          ref: "Comment"
