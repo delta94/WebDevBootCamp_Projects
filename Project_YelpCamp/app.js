0ad326258dbadf4075d2ce0cb00088aeb48aa9b3
Piyali Dey <pdey@ncsu.edu>
2017-03-07 13:09:39 -0800

    Added DATABASEURL

--- Project_YelpCamp/app.js
+++ Project_YelpCamp/app.js
@@ -16,7 +16,13 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+// Connect to mongodb
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(url);
+
+//mongoose.connect("mongodb://localhost/yelp_camp_v10");
+//mongoose.connect("mongodb://piyali:123456@ds123050.mlab.com:23050/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -47,7 +53,11 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
+/*Heroku will need us to run on a specific port, which we access through 
+the process object, which is available to use. Make your app listen on 
+the port number specified in process.env.PORT, or if it can’t find one, 
+revert to 3000. This way it will work both locally and on Heroku.*/
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
    console.log("The YelpCamp Server Has Started!");
 });
